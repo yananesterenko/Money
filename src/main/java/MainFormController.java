165ce6c342bd334e791acfdd39b54061e8d26ee8
ynesterenko <yana.nesterenko@gmail.com>
2018-06-20 17:33:37 +0300

    First

---
+++ src/main/java/MainFormController.java
@@ -0,0 +1,13 @@
+import javafx.stage.Stage;
+
+public class MainFormController{
+
+    public void createMainForm(){
+        Stage st = new Stage();
+        st.setTitle("Main form");
+        st.show();
+
+        }
+
+
+}
